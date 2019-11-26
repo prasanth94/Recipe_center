76966658e438da663b95d3a0537aa98f9c8627fd
Prasanth M <mprasanthk27@gmail.com>
2019-11-26 18:50:57 +0530

    Search Results View

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,38 @@
-// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
+
+
+/* Global state of the app
+1. search object
+2. current recipe
+3.shopping list object
+4.Liked recipe
+*/
+const state = {};
+
+const controlSearch = async () => {
+// 1. Get the query from the viewport
+const query = searchView.getInput();
+
+if(query){
+  //2. New search object and add to state
+  state.search = new Search(query);
+
+  // 3.Prepare UI for results
+  searchView.clearInput();
+  searchView.clearResults();
+  
+  //4. Search for recipes
+  await state.search.getResults();
+
+  //5. Render Results
+  searchView.renderResults(state.search.result);
+}
+
+}
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
